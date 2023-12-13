72be6db77b30e0068e88177812cafeb753bf4c26
Ilya Kazancev <97786849+IVKazantsev@users.noreply.github.com>
2023-12-13 15:16:09 +0200

    fixed bugs

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -8,29 +8,6 @@ using namespace entity;
 int main() {
     Workflow workflow;
     Keyboard keyboard(workflow);
-    keyboard.PressKey("Caps Lock");
-    keyboard.PressKey("h");
-    keyboard.PressKey("Caps Lock");
-    keyboard.PressKey("e");
-    keyboard.PressKey("l");
-    keyboard.PressKey("l");
-    keyboard.PressKey("o");
-
-    keyboard.PressKey("Tab");
-    keyboard.PressKeys("Shift+A");
-    keyboard.PressKey("+");
-
-    keyboard.PressKey("Enter");
-    keyboard.PressKeys("Enter");
-    keyboard.PressKey("!");
-    keyboard.PressKey("Backspace");
-
-    keyboard.printCommandList();
-
-    keyboard.reassignKey("P", "Back");
-    keyboard.reassignKey("Backspace", "Return");
-
-    keyboard.printWorkflow();
-    keyboard.printCommandList();
+    keyboard.launch();
     return 0;
 }
