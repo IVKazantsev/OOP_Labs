dc15e34a890438a1cccc8b52f9897429bbbd7631
Ilya Kazancev <97786849+IVKazantsev@users.noreply.github.com>
2023-12-13 15:16:27 +0200

    fixed bugs

--- Lab3/src/Keyboard.cpp
+++ Lab3/src/Keyboard.cpp
@@ -2,46 +2,16 @@
 // Created by Honor on 25.10.2023.
 //
 
-#include "../headers/Keyboard.h"
+#include <utility>
 
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include "../headers/Keyboard.h"
 
 using namespace entity;
 
-Keyboard::Keyboard(Workflow workflow) : m_workflow(workflow) {
+Keyboard::Keyboard(Workflow workflow) : m_workflow(std::move(workflow)) {
 
-};
-
-void Keyboard::PressKey(const std::string &key) {
-    if (key.length() == 0) {
-        std::cout << "Вы ввели не клавишу";
-        return;
-    } else if (key.length() > 1) {
-        m_workflow.enroll(key, m_workflow.getCommandOrFail(key));
-        return;
-    }
-
-    m_workflow.enroll(key, "key");
 }
 
-void Keyboard::PressKeys(const std::string &keys) {
-    if (find(keys.begin(), keys.end(), '+') != keys.end()) {
-        m_workflow.enroll(keys, "combination");
-        return;
-    }
-    PressKey(keys);
-}
-
-void Keyboard::printWorkflow() {
-    m_workflow.printWorkflow();
-}
-
-void Keyboard::printCommandList() {
-    m_workflow.printCommandList();
-}
-
-void Keyboard::reassignKey(const std::string &key, const std::string &command) {
-    m_workflow.reassignKey(key, command);
+void Keyboard::launch() {
+    m_workflow.launch();
 }
--- Lab3/src/Workflow.cpp
+++ Lab3/src/Workflow.cpp
@@ -6,57 +6,83 @@
 
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace entity;
 
 Workflow::Workflow() {
-    m_commandList.emplace_back("Upper Case", "Caps Lock");
-    m_commandList.emplace_back("Tabulation", "Tab");
-    m_commandList.emplace_back("Enter", "Enter");
-    m_commandList.emplace_back("Back", "Backspace");
-}
-
-void Workflow::enroll(const std::string &key, const std::string &appointment) {
-    m_sequencing.emplace_back(key, appointment);
-    printPressing();
-}
-
-void Workflow::printPressing() {
-    std::cout << m_sequencing[m_sequencing.size() - 1].first
-              << ": " << m_sequencing[m_sequencing.size() - 1].second
-              << std::endl;
-}
-
-void Workflow::printWorkflow() {
-    std::cout << "Temp WorkFlow" << std::endl;
-    for (const auto& press: m_sequencing) {
-        std::cout << "'" << press.first << "' ";
-    }
-    std::cout << std::endl;
+    m_commandList.emplace_back("backspace", "back");
+    m_commandList.emplace_back("ctrl+C", "copy");
+    m_commandList.emplace_back("ctrl+V", "paste");
 }
 
 void Workflow::printCommandList() {
     std::cout << "Temp Command List" << std::endl;
-    for (const auto& command: m_commandList) {
-        std::cout << "Command: " << command.first << ", key: " << command.second << std::endl;
+    for (Command command: m_commandList) {
+        std::cout << "Command: " << command.getAction() << ", key: " << command.getKey() << std::endl;
     }
 }
 
-void Workflow::reassignKey(const std::string &key, const std::string &command) {
-    for(int i = 0; i < m_commandList.size(); i++) {
-        if(m_commandList[i].first == command) {
-            m_commandList[i].second = key;
+void Workflow::reassignKey(const std::string &oldKey, const std::string &newKey) {
+    for (auto & command : m_commandList) {
+        if (command.getKey() == oldKey) {
+            command.setKey(newKey);
             return;
         }
     }
-    m_commandList.emplace_back(command, key);
 }
 
-std::string Workflow::getCommandOrFail(const std::string &key) {
-    for (const auto& commandItem: m_commandList) {
-        if (commandItem.second == key) {
-            return commandItem.first;
+std::string Workflow::getAction(const std::string &key) {
+    for (auto & command: m_commandList) {
+        if (command.getKey() == key) {
+            return command.getAction();
         }
     }
-    return "Unknown command";
+
+    return "";
+}
+
+void Workflow::launch() {
+    std::string stringForCopy;
+
+    while (true) {
+        std::cout << "Введите клавишу или команду" << std::endl;
+        std::cout << "(exit | reassign | showCommands)" << std::endl;
+        std::string key;
+        std::cin >> key;
+
+        if (key == "exit") {
+            break;
+        }
+
+        if (key == "showCommands") {
+            printCommandList();
+        } else if (key == "reassign") {
+            std::cout << "Введите клавишу, которую хотите переназначить" << std::endl;
+            std::string oldKey;
+            std::cin >> oldKey;
+
+            std::cout << "Введите новую клавишу" << std::endl;
+            std::string newKey;
+            std::cin >> newKey;
+
+            reassignKey(oldKey, newKey);
+        } else if (getAction(key) == "back") {
+            if (!m_sequencing.empty()) {
+                m_sequencing.pop_back();
+            }
+        } else {
+            if (getAction(key) == "copy") {
+                stringForCopy = m_sequencing;
+            } else if (getAction(key) == "paste") {
+                if (!stringForCopy.empty()) {
+                    m_sequencing += stringForCopy;
+                }
+            } else {
+                m_sequencing += key;
+            }
+        }
+        std::cout << m_sequencing << std::endl;
+        Sleep(1000);
+    }
 }
